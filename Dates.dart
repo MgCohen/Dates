f7be56c5b4f4f136787ede73a0b7eebfab4badbd
MgCohen <44907436+MgCohen@users.noreply.github.com>
2020-06-03 20:01:31 -0300

    Add files via upload

---
+++ Dates.dart
@@ -0,0 +1,12 @@
+
+
+List<DateTime> usedDates = [
+//  DateTime.now().subtract(new Duration(days: 0)),
+//  DateTime.now().add(new Duration(days: 0)),
+
+  DateTime.now().add(new Duration(days: 4)),
+  DateTime.now().add(new Duration(days: 5)),
+  DateTime.now().add(new Duration(days: 9)),
+  DateTime.now().add(new Duration(days: 10)),
+];
+
